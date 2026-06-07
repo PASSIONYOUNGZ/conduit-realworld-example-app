7beee1052195011550c7a6cf61df553c5b232ec5
PassionYoungz <2385862751@qq.com>
2026-06-07 16:28:59 +0800

    feat: add profile about me tab

--- frontend/src/routes/Profile/Profile.jsx
+++ frontend/src/routes/Profile/Profile.jsx
@@ -5,7 +5,7 @@ import ContainerRow from "../../components/ContainerRow";
 import NavItem from "../../components/NavItem";
 
 function Profile() {
-  const { state } = useLocation();
+  const { state, pathname } = useLocation();
   const { username } = useParams();
   const { loggedUser } = useAuth();
 
@@ -21,14 +21,17 @@ function Profile() {
         <div className="col-xs-12 col-md-10 offset-md-1">
           <div className="articles-toggle">
             <ul className="nav nav-pills outline-active">
-              {loggedUser?.username === username && (
-                <NavItem text="About Me" url="about" state={state} />
-              )}
-              <NavItem text="My Articles" url="" state={state} />
+              <NavItem text="About Me" url="about" state={state} />              <NavItem text="My Articles" url="" state={state} />
               <NavItem text="Favorited Articles" url="favorites" state={state} />
             </ul>
           </div>
-          <Outlet />
+          {pathname.endsWith('/about') ? (
+            <div className="about-me-content p-4">
+              {state?.bio ? <p>{state.bio}</p> : <p className="text-muted">暂无个人简介</p>}
+            </div>
+          ) : (
+            <Outlet />
+          )}
         </div>
       </ContainerRow>
     </div>
